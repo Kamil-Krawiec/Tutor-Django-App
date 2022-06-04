a65185cfd015861a3f815420f27ee78add0f1bef
Kamil Krawiec <kamil.krawiec9977@gmail.com>
2022-06-04 17:42:55 +0200

    Login/register background added

--- JSProjekt/settings.py
+++ JSProjekt/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS='bootstrap5'
+CRISPY_TEMPLATE_PACK='bootstrap5'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -126,3 +132,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = '/home'
+LOGOUT_REDIRECT_URL = '/login'
--- JSProjekt/urls.py
+++ JSProjekt/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('',include('django.contrib.auth.urls')),
+    path('home',views.home, name = 'home'),
+    path('sign-up',views.sign_up, name = 'sing_up')
+
 ]
